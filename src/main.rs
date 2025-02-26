029e59f956707a99f37694a6617a5bb5a6a27deb
Etienne Le Louët <etiennelelouet@outlook.com>
2025-02-27 00:01:07 +0100

    improved timer handling logic

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ pub mod wl_app;
 pub mod output;
 pub mod memory;
 pub mod background_image;
+pub mod image_file;
 
 use nix::sys::epoll;
 use wl_app::WlApp;
@@ -9,20 +10,18 @@ use wayland_client::{protocol::{wl_display::WlDisplay, wl_shm}, ConnectError::*,
 use std::{collections::HashMap, time::{Duration, Instant}};
 
 fn main() {
-    let file_paths = ["/home/etienne/Downloads/bigmontparnasse.webp", "/home/etienne/Downloads/montparnasse.jpg"];
-    let bg_duration_as_duration = Duration::new(5, 0);
-    let mut next_timer = bg_duration_as_duration;
+    let image_list = image_file::get_image_list(String::from("/home/etienne/Pictures/wallpaper/"));
+    if image_list.len() == 0 {
+	panic!("no images to set as background !");
+    }
+
+    let bg_duration_as_duration = Duration::new(15, 0);
+    let mut next_timer = None;
     let bg_duration_as_epoll_timer = match epoll::EpollTimeout::try_from(bg_duration_as_duration.as_millis()) {
         Ok(duration) => duration,
         Err(error) => panic!("Time between updates is incorrect: {}", error),
     };
     let acceptable_delta = Duration::new(1, 0);
-
-
-    let epoll = match epoll::Epoll::new(epoll::EpollCreateFlags::empty()) {
-        Ok(epoll) => epoll,
-        Err(error) => panic!("error when creating an epoll instance: {}", error)
-    };
 
     let mut wl_app: wl_app::WlApp = wl_app::WlApp {
 	output_map: HashMap::new(),
@@ -73,6 +72,13 @@ fn main() {
 	panic!("could not find Argb8888 in supported formats vec");
     }
 
+    let epoll = match epoll::Epoll::new(epoll::EpollCreateFlags::empty()) {
+	Ok(epoll) => epoll,
+	Err(error) => panic!("error when creating an epoll instance: {}", error)
+    };
+
+    let mut index_in_image_array = 0;
+
     loop {
 	let read_guard = event_queue.prepare_read().unwrap();
 	let fd = read_guard.connection_fd();
@@ -94,16 +100,22 @@ fn main() {
 	    panic!("error when flushing event queue : {}", error);
 	}
 
-	let timeout = match epoll::EpollTimeout::try_from(next_timer) {
+	if let None = next_timer {
+	    next_timer = Some(bg_duration_as_duration);
+	}
+
+	let timeout = match epoll::EpollTimeout::try_from(next_timer.unwrap()) {
 	    Ok(timeout) => timeout,
 	    Err(_) => bg_duration_as_epoll_timer,
 	};
+
+	println!("epoll wait for {:#?}", next_timer.unwrap());
 
 	let nfd = match epoll.wait(&mut events, timeout) {
 	    Ok(res) => res,
 	    Err(epollerror) => panic!("error when waiting on epoll: {}", epollerror)
 	};
-
+	println!("#########################################epoll wait finished#########################################");
 	if nfd > 0 {{
 	    let n_events = match read_guard.read() {
 		Ok(n_events) => n_events,
@@ -121,23 +133,29 @@ fn main() {
 	}}
 
 	let now = Instant::now();
-
+	next_timer = None;
 	for (key,output) in wl_app.output_map.iter_mut() {
 	    if output.should_update_config == false {
 		match output.next_redraw {
 		    Some(next_redraw) => {
 			if now + acceptable_delta < next_redraw {
 			    let next_redraw_delta = next_redraw - now;
-			    if next_redraw_delta < next_timer {
-				next_timer = next_redraw_delta;
+			    println!("output {}, next_redraw_delta: {:#?}", output.name, next_redraw_delta);
+			    if let None = next_timer {
+				next_timer = Some(next_redraw_delta);
+			    }
+			    if next_redraw_delta < next_timer.unwrap() {
+				next_timer = Some(next_redraw_delta);
 			    }
 			    continue;
 			}
 		    },
 		    None => (),
 		}
-		output.render(key, &qh);
-		output.next_redraw = Some(now + bg_duration_as_duration);
+
+		output.render(key, &qh, Some(&(image_list.get(index_in_image_array).unwrap().path)));
+		output.next_redraw = Some(Instant::now() + bg_duration_as_duration);
+		index_in_image_array = (index_in_image_array + 1) % image_list.len();
 	    }
 	}
     }
