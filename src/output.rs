a342f18ce0e748f98aeea3eb2e7fc2e127082614
Etienne Le Louët <etiennelelouet@outlook.com>
2025-04-29 18:07:54 +0200

    factorised and reformated code

--- src/output.rs
+++ src/output.rs
@@ -1,7 +1,14 @@
+use crate::{
+    background_image::{fill_buffer_random, fill_buffer_with_image, BackgroundImageError},
+    memory::MemoryMapping,
+    wl_app::WlApp,
+};
 use std::{num::NonZeroUsize, os::fd::AsFd, path::PathBuf, time::Instant};
-use wayland_client::{protocol::{wl_buffer, wl_output, wl_shm, wl_shm_pool, wl_surface}, QueueHandle};
+use wayland_client::{
+    protocol::{wl_buffer, wl_output, wl_shm, wl_shm_pool, wl_surface},
+    QueueHandle,
+};
 use wayland_protocols_wlr::layer_shell::v1::client::zwlr_layer_surface_v1;
-use crate::{background_image::{fill_buffer_with_image, fill_buffer_random}, memory::MemoryMapping, wl_app::WlApp};
 
 #[derive(Debug)]
 pub struct Output {
@@ -21,147 +28,156 @@ pub struct Output {
     pub should_update_config: bool,
     pub next_redraw: Option<Instant>,
     pub image_list: Vec<usize>,
-    pub index_in_image_list: usize
+    pub index_in_image_list: usize,
 }
 
 impl Output {
     pub fn new() -> Self {
-	Self{
-	    make: String::from(""),
-	    model: String::from(""),
-	    name: String::from(""),
-	    mode_height: 0,
-	    mode_width: 0,
-	    description: String::from(""),
-	    wl_output_proxy: None,
-	    wl_surface_proxy: None,
-	    wlr_layer_surface_proxy: None,
-	    wl_shm_pool: None,
-	    wl_buffer: None,
-	    mapping: None,
-	    serial_to_ack: 0,
-	    should_update_config: true,
-	    next_redraw: None,
-	    image_list: Vec::new(),
-	    index_in_image_list: 0,
-	}
+        Self {
+            make: String::from(""),
+            model: String::from(""),
+            name: String::from(""),
+            mode_height: 0,
+            mode_width: 0,
+            description: String::from(""),
+            wl_output_proxy: None,
+            wl_surface_proxy: None,
+            wlr_layer_surface_proxy: None,
+            wl_shm_pool: None,
+            wl_buffer: None,
+            mapping: None,
+            serial_to_ack: 0,
+            should_update_config: true,
+            next_redraw: None,
+            image_list: Vec::new(),
+            index_in_image_list: 0,
+        }
     }
 
     pub fn get_memory_size(&self) -> NonZeroUsize {
-	let shm_pool_size_usize = match usize::try_from(self.mode_width * self.mode_height * 4) {
-	    Ok(shm_pool_size_usize) => shm_pool_size_usize,
-	    Err(err) => panic!("failed to convert shm_pool_size to usize: {}", err)
-	};
-	match NonZeroUsize::new(shm_pool_size_usize) {
-	    Some(shm_pool_size_nonzerousize) => shm_pool_size_nonzerousize,
-	    None => panic!("failed to convert shm_pool_size to non zero usize")
-	}
+        let shm_pool_size_usize = match usize::try_from(self.mode_width * self.mode_height * 4) {
+            Ok(shm_pool_size_usize) => shm_pool_size_usize,
+            Err(err) => panic!("failed to convert shm_pool_size to usize: {}", err),
+        };
+        match NonZeroUsize::new(shm_pool_size_usize) {
+            Some(shm_pool_size_nonzerousize) => shm_pool_size_nonzerousize,
+            None => panic!("failed to convert shm_pool_size to non zero usize"),
+        }
     }
 
     pub fn get_stride_i32(&self) -> i32 {
-	match i32::try_from(self.mode_width * 4) {
-	    Ok(stride) => stride,
-	    Err(err) => panic!("failed to convert stride to i32: {}", err)
-	}
+        match i32::try_from(self.mode_width * 4) {
+            Ok(stride) => stride,
+            Err(err) => panic!("failed to convert stride to i32: {}", err),
+        }
     }
 
     pub fn get_memory_size_i32(&self) -> i32 {
-	match i32::try_from(self.get_memory_size().get()) {
-	    Ok(shm_pool_size_i32) => shm_pool_size_i32,
-	    Err(err) => panic!("failed to convert shm_pool_size to i32: {}", err)
-	}
+        match i32::try_from(self.get_memory_size().get()) {
+            Ok(shm_pool_size_i32) => shm_pool_size_i32,
+            Err(err) => panic!("failed to convert shm_pool_size to i32: {}", err),
+        }
     }
 
-    pub fn configure_shm_pool(&mut self, key: &u32, wl_shm_proxy: &wl_shm::WlShm, qhandle: &QueueHandle<WlApp>) {
-	self.mapping = match MemoryMapping::new(key.to_string(), self.get_memory_size()) {
-	    Some(mapping) => Some(mapping),
-	    None => panic!("Creating buffer failed !"),
-	};
+    pub fn configure_shm_pool(
+        &mut self,
+        key: &u32,
+        wl_shm_proxy: &wl_shm::WlShm,
+        qhandle: &QueueHandle<WlApp>,
+    ) {
+        self.mapping = match MemoryMapping::new(key.to_string(), self.get_memory_size()) {
+            Some(mapping) => Some(mapping),
+            None => panic!("Creating buffer failed !"),
+        };
 
-	self.wl_shm_pool =  Some(
-	    wl_shm_proxy.create_pool(
-		self.mapping.as_ref().unwrap().fd.as_fd(), self.get_memory_size_i32(), qhandle, *key
-	    )
-	);
+        self.wl_shm_pool = Some(wl_shm_proxy.create_pool(
+            self.mapping.as_ref().unwrap().fd.as_fd(),
+            self.get_memory_size_i32(),
+            qhandle,
+            *key,
+        ));
     }
 
-    pub fn render(&mut self, key: &u32, qhandle: &QueueHandle<WlApp>, to_draw: Option<&PathBuf>) {
-	let wl_shm_pool = self.wl_shm_pool.as_ref().unwrap();
-	self.wl_buffer = Some(
-	    wl_shm_pool.create_buffer(
-		0,
-		self.mode_width as i32,
-		self.mode_height as i32,
-		self.get_stride_i32(),
-		wl_shm::Format::Argb8888,
-		qhandle,
-		*key
-	    )
-	);
+    pub fn create_buffer(
+        &mut self,
+        key: &u32,
+        qhandle: &QueueHandle<WlApp>,
+    ) {
+        let wl_shm_pool = self.wl_shm_pool.as_ref().unwrap();
+        self.wl_buffer = Some(wl_shm_pool.create_buffer(
+            0,
+            self.mode_width as i32,
+            self.mode_height as i32,
+            self.get_stride_i32(),
+            wl_shm::Format::Argb8888,
+            qhandle,
+            *key,
+        ));
+    }
 
-	let buffer = self.wl_buffer.as_ref().unwrap();
-	let ptr: & mut[u8];
-	let mapping = self.mapping.as_ref().unwrap();
-	unsafe {
-	    ptr = std::slice::from_raw_parts_mut::<u8>(
-		mapping.ptr.as_ptr() as *mut u8,
-		mapping.size.get()
-	    );
+    pub fn fill_buffer_dispatch(&self, to_draw: &Option<&PathBuf>, ptr: &mut [u8]) -> Result<(), BackgroundImageError> {
+        match to_draw {
+            Some(path) => {
+                return fill_buffer_with_image(&path, self.mode_width, self.mode_height, ptr);
+            }
+            None => {
+                return fill_buffer_random(ptr);
+            }
+        }
+    }
+
+    pub fn render(
+        &mut self,
+        key: &u32,
+        qhandle: &QueueHandle<WlApp>,
+        to_draw: Option<&PathBuf>,
+    ) -> Result<(), BackgroundImageError> {
+        self.create_buffer(key, qhandle);
+        let ptr: &mut [u8];
+
+        unsafe {
+            ptr = std::slice::from_raw_parts_mut::<u8>(
+                self.mapping.as_ref().unwrap().ptr.as_ptr() as *mut u8,
+                self.mapping.as_ref().unwrap().size.get(),
+            );
+        }
+
+        if let Err(error) = self.fill_buffer_dispatch(&to_draw, ptr) {
+	    return Err(error);
 	}
-	// before the panic!, if the "Some" arm failed, the "None" was called automatically ??
-	let _ = match to_draw {
-	    Some(path) => {
-		println!("calling fill_buffer_with_image for output {}", self.name);
-		match fill_buffer_with_image(
-		    &path,
-		    self.mode_width,
-		    self.mode_height,
-		    ptr
-		) {
-		    Ok(_) => (),
-		    Err(error) => panic!("fill_buffer_with_image failed: {}", error)
-		}
-	    },
-	    None => {
-		println!("calling fill_buffer_random for output {}", self.name);
-		match fill_buffer_random(ptr) {
-		    Ok(_) => (),
-		    Err(error) => panic!("fill_buffer_random failed: {}", error),
-		}
-	    },
-	};
 
 	let surface = self.wl_surface_proxy.as_ref().unwrap();
-	surface.set_buffer_scale(1);
-	surface.attach(Some(buffer), 0, 0);
-	surface.damage_buffer(0, 0, i32::MAX, i32::MAX);
-	surface.commit();
+        surface.set_buffer_scale(1);
+        surface.attach(Some(self.wl_buffer.as_ref().unwrap()), 0, 0);
+        surface.damage_buffer(0, 0, i32::MAX, i32::MAX);
+        surface.commit();
+	return Ok(());
     }
 
     pub fn clear(&mut self) {
-	if let Some(ref layer_shell) = self.wlr_layer_surface_proxy {
-	    layer_shell.destroy();
-	    self.wlr_layer_surface_proxy = None;
-	}
-	if let Some(ref surface_proxy) = self.wl_surface_proxy {
-	    surface_proxy.destroy();
-	    self.wl_surface_proxy = None;
-	}
-	if let Some(ref wl_shm_pool) = self.wl_shm_pool {
-	    wl_shm_pool.destroy();
-	    self.wl_shm_pool = None;
-	}
-	if let Some(ref wl_buffer) = self.wl_buffer {
-	    wl_buffer.destroy();
-	    self.wl_buffer = None;
-	}
-	if let Some(ref mapping) = self.mapping {
-	    if let Err(error) = mapping.destroy() {
-		panic!("mapping destroy error {}", error);
-	    }
-	    self.mapping = None;
-	}
-	self.should_update_config = true;
-	self.next_redraw = None;
+        if let Some(ref layer_shell) = self.wlr_layer_surface_proxy {
+            layer_shell.destroy();
+            self.wlr_layer_surface_proxy = None;
+        }
+        if let Some(ref surface_proxy) = self.wl_surface_proxy {
+            surface_proxy.destroy();
+            self.wl_surface_proxy = None;
+        }
+        if let Some(ref wl_shm_pool) = self.wl_shm_pool {
+            wl_shm_pool.destroy();
+            self.wl_shm_pool = None;
+        }
+        if let Some(ref wl_buffer) = self.wl_buffer {
+            wl_buffer.destroy();
+            self.wl_buffer = None;
+        }
+        if let Some(ref mapping) = self.mapping {
+            if let Err(error) = mapping.destroy() {
+                panic!("mapping destroy error {}", error);
+            }
+            self.mapping = None;
+        }
+        self.should_update_config = true;
+        self.next_redraw = None;
     }
 }
