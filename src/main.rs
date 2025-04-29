1f89ee1cd5c55ae302094b6b8d9e995535f7d9b5
Etienne Le Louët <etiennelelouet@outlook.com>
2025-04-29 18:07:05 +0200

    removed old tests

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ pub mod image_order_prio;
 pub mod test;
 
 use config::Config;
-// use wl_app::WlApp;
+use wl_app::WlApp;
 
 fn main() {
     let config = Config::get_config();
@@ -17,11 +17,6 @@ fn main() {
 	panic!("No images to set as background !");
     }
 
-    test::test_best_fit(&image_list, config.best_fit_fact);
-    println!("ici");
-    test::test_next_file(&image_list);
-
-
-    // let mut wl_app = WlApp::new(config, image_list);
-    // wl_app.run();
+    let mut wl_app = WlApp::new(config, image_list);
+    wl_app.run();
 }
